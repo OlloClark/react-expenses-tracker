781fc3a20790ef1b849006b6561e0a4403748eac
OlloClark <ollo42@hotmail.co.uk>
2023-03-13 13:46:37 -0700

    moving date to ExpenseDate.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem({ title, date, amount }) {
-  const month = date.toLocaleString("en-US", { month: "short" });
-  const day = date.toLocaleString("eng-US", { day: "2-digit" });
-  const year = date.getFullYear();
+function ExpenseItem({ title, amount, date }) {
+  
 
   return (
     <div className="expense-item">
+
       <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+        <ExpenseDate date={date}/>
       </div>
+      
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>

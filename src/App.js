00084d89b99a455ce457331db2d788262f0d59fb
OlloClark <ollo42@hotmail.co.uk>
2023-03-20 16:01:44 -0700

    stubbing submitHandler()

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
   ];
 
   return (
-
     <div>
       <NewExpense />
       <Expenses expenseData={expenseData} />
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+  const [inputDate, setInputDate] = useState("");
+
+  function titleChangeHandler(event) {
+    setInputTitle(event.target.value);
+  }
+
+  function amountChangeHandler(event) {
+    setInputAmount(event.target.value);
+  }
+
+  function dateChangeHandler(event) {
+    setInputDate(event.target.value);
+  }
+
+  function submitHandler() {
+
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2019-01-01"
+            max="2023-12-31"
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-		<button type="submit">Add Expense</button>
-	  </div>
+        <button type="submit">Add Expense</button>
+      </div>
     </form>
   );
 }

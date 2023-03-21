3e98b99da0a42b2a9d72b768a0252fd33f43bfbb
OlloClark <ollo42@hotmail.co.uk>
2023-03-21 16:33:47 -0700

    removing title practice button ExpenseItem.js

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './ExpensesFilter.css';
+import "./ExpensesFilter.css";
 
 function ExpensesFilter(props) {
-
   function dropdownChangeHandler(event) {
-    props.onFilterChange(event.target.value)
- 
+    props.onFilterChange(event.target.value);
   }
 
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
   );
-};
+}
 
 export default ExpensesFilter;

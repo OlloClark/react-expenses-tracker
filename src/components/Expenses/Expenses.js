e4969d3127ca9417d4e240d8b6aea14f900a9f0f
OlloClark <ollo42@hotmail.co.uk>
2023-03-21 16:00:09 -0700

    adding ExpensesFilter.js/css, passing selectedYear to Expenses as state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import "./Expenses.css"
+import "./Expenses.css";
 
 function Expenses({ expenseData }) {
+  const [filteredYear, setFilteredYear] = useState("");
+
+  function filterChangeHandler(selectedYear) {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  }
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenseData[0].title}
-        amount={expenseData[0].amount}
-        date={expenseData[0].date}
-      />
-      <ExpenseItem
-        title={expenseData[1].title}
-        amount={expenseData[1].amount}
-        date={expenseData[1].date}
-      />
-      <ExpenseItem
-        title={expenseData[2].title}
-        amount={expenseData[2].amount}
-        date={expenseData[2].date}
-      />
-      <ExpenseItem
-        title={expenseData[3].title}
-        amount={expenseData[3].amount}
-        date={expenseData[3].date}
-      />
-    </Card>
+
+      <Card className="expenses">
+        <ExpensesFilter onFilterChange={filterChangeHandler} />
+        <ExpenseItem
+          title={expenseData[0].title}
+          amount={expenseData[0].amount}
+          date={expenseData[0].date}
+        />
+        <ExpenseItem
+          title={expenseData[1].title}
+          amount={expenseData[1].amount}
+          date={expenseData[1].date}
+        />
+        <ExpenseItem
+          title={expenseData[2].title}
+          amount={expenseData[2].amount}
+          date={expenseData[2].date}
+        />
+        <ExpenseItem
+          title={expenseData[3].title}
+          amount={expenseData[3].amount}
+          date={expenseData[3].date}
+        />
+      </Card>
+
   );
 }
 
